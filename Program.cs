848b4a08b47a099c6b2bb0681ea613ed4d7932ea
RokiWielki <Cruck222@interia.pl>
2023-01-08 13:25:08 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Polecenie_Replit
+{
+
+    public interface IPolecenie
+    {
+        public void wykonaj();
+    }
+
+    public class KomendaWlacz : IPolecenie
+    {
+        Lampa lampa;
+        public KomendaWlacz(Lampa lampa)
+        {
+            this.lampa = lampa;
+        }
+        public void wykonaj()
+        {
+            lampa.wlacz();
+        }
+    }
+
+    public class KomendaWylacz : IPolecenie
+    {
+        Lampa lampa;
+        public KomendaWylacz(Lampa lampa)
+        {
+            this.lampa= lampa;
+        }
+        public void wykonaj()
+        {
+            lampa.wylacz();
+        }
+    }
+
+
+    public class Lampa
+    {
+        bool stan= false;
+        public void wlacz()
+        {
+            this.stan= true;
+        }
+
+        public void wylacz()
+        {
+            this.stan=false;
+        }
+
+        public bool sprawdz()
+        {
+            return stan;
+        }
+    }
+
+
+    public class Pilot
+    {
+        private IPolecenie polecenie;
+
+        public void ustawPolecenie(IPolecenie polecenie)
+        {
+            this.polecenie=polecenie;
+        }
+
+        public void wcisnijGuzik()
+        {
+            polecenie.wykonaj();
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampa lampa=new Lampa();
+            Pilot pilot =new Pilot();
+
+            IPolecenie wlacz = new KomendaWlacz(lampa);
+            IPolecenie wylacz = new KomendaWylacz(lampa);
+
+            Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+
+            pilot.ustawPolecenie(wlacz);
+            pilot.wcisnijGuzik();
+            Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+
+            pilot.ustawPolecenie(wylacz);
+            pilot.wcisnijGuzik();
+            Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+
+        }
+    }
+}
